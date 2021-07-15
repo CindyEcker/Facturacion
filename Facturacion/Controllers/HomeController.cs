2db23fbe0977006de6a49d3a82a6620a45aaceee
CindyEcker <cindy_ecker@hotmail.com>
2021-07-15 16:41:33 -0400

    Login y filtro

--- Facturacion/Controllers/HomeController.cs
+++ Facturacion/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Facturacion.Models;
+using Facturacion.Filters;
 
 namespace Facturacion.Controllers
 {
+    [VerificarSession]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- Facturacion/Startup.cs
+++ Facturacion/Startup.cs
@@ -25,6 +25,7 @@ namespace Facturacion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddMvc();
             services.AddDbContext<FacturacionDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("conn")));
@@ -43,6 +44,8 @@ namespace Facturacion
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
