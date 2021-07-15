d868b24e3a23363bda0dc32091d41e8f66371349
CindyEcker <cindy_ecker@hotmail.com>
2021-07-15 16:47:51 -0400

    Arreglo css

--- Facturacion/Models/Vendedor.cs
+++ Facturacion/Models/Vendedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace Facturacion.Models
     {
         public int ID { get; set; }
         public string Nombre { get; set; }
+        [Display(Name="Porcentaje de comisión")]
         public int Porc_Comision { get; set; }
         [ForeignKey("Estado")]
+        [Display(Name = "Estado")]
         public int ID_Estado { get; set; }
 
         public Estado Estado { get; set; }
